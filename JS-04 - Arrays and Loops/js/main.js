6f603fd0c85462ad456d0f81b858595495da673d
ernt <ernestn@ciencias.unam.mx>
2023-03-28 07:32:39 -0600

    Final Exercise #3

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -39,13 +39,45 @@ for (let i = 0; i <= 5; i++) {
 /* Pendiente al revisar */
 
 /* Exercise #3 */
+
 /* – Repeatedly print the value of the variable xValue, decreasing it by 0.5 each time,
 as long as xValue remains positive. */
+let n=10;
+while (n>0) {
+    n-=0.5;
+    console.log(n);
+}
+
+
 /* - Print all the odd numbers between 1 - 100. */
+let n1=1
+while (n1<100) {
+    n1++;
+    if (n1%2==0) {
+        console.log(n1);
+    }
+    
+}
 /* - Write a method with a while loop to print 1 through n in square brackets. 
 For example, if n = 6 print [1] [2] [3] [4] [5] [6] */
+let n2=100;
+while (n2>=1) {
+    
+    console.log("["+n2+"]");
+    n2--;
+}
 /* - Write a method with a while loop that computes the sum of first n positive integers: 
 sum = 1 + 2 + 3 + … + n
 Examples:
 n = 5 sum = 15
 n = 19 sum = 190 */
+
+let n3=19;
+let sum=0;
+while (n3>0) {
+    
+    sum+=n3--;
+    
+    
+}
+console.log(sum);
